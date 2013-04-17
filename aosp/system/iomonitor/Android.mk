09360282c90872bb3678c2648e1fdad3531ffb6c
Chris Long <christopherglong@mac.com>
2013-04-17 13:44:30 -0400

    Rename userland receiving module

--- aosp/system/iomonitor/Android.mk
+++ aosp/system/iomonitor/Android.mk
@@ -3,5 +3,5 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := iomonitor.c
 LOCAL_SHARED_LIBRARIES := libc
-LOCAL_MODULE := objstr_main
+LOCAL_MODULE := iomonitor
 include $(BUILD_EXECUTABLE)
